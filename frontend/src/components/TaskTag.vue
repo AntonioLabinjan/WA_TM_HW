7406a891806e981fd36ae350a37133b2c1554e54
Antonio Labinjan <alabinjan6@gmail.com>
2025-01-16 20:53:49 +0100

    Add files via upload

---
+++ frontend/src/components/TaskTag.vue
@@ -0,0 +1,28 @@
+<template>
+    <span :class="tagMapping[tag]">{{ tag }}</span>
+  </template>
+  
+  <script setup>
+  defineProps({
+    tag: Object,
+    //className: String, 
+  });
+
+  let tagMapping = {
+    "hitno": "bg-red-500 text-white",
+    "faks": "bg-blue-500 text-white",
+    "pomalo": "bg-green-500 text-white"
+  }
+  </script>
+  
+  <style scoped>
+  .tag {
+    padding: 0.5rem;
+    border-radius: 0.25rem;
+    font-size: 0.875rem;
+    display: inline-block;
+    margin-right: 0.5rem;
+    margin-bottom: 0.5rem;
+  }
+  </style>
+  
